ce676acb9a3af781302a6ff4b41395c2b55c35f5
Revanth Ratna Kireeti Karri <39022055+KRRKireeti@users.noreply.github.com>
2024-05-15 17:14:01 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Revanth Ratna Kireeti Karri",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Security Operations Enthusiast",
+    bio: "",
+    email: "k.revanthratnakireeti@gmail.com",
+    linkedin: "KRRKireeti",
+    github: "KRRKireeti",
+    twitter: "KRRKireeti",
   },
   projects: [
     {
